b8baff325c54369e414bd1e8adfa53facacbd134
ofekrv <ofekrv@gmail.com>
2020-07-20 08:52:46 +0300

    integ + unit

--- src/main/java/highscore/services/BestScoreService.java
+++ src/main/java/highscore/services/BestScoreService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "jplaceholder", url = "$BEST_SCORE_URL")
+@FeignClient(value = "BestScore", url = "$BEST_SCORE_URL")
 public interface BestScoreService {
     @RequestMapping(method = RequestMethod.POST, value = "/submitNewHighest")
     public void submitNewHighest(Highscore score);
